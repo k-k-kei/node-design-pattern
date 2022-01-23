824b1596ccdcab759ca94dc084fe5417b6bd9224
keita_kawaguchi <keikawagu@gmail.com>
2022-01-23 18:44:11 +0900

    継続渡しと継続渡しではないコールバック

--- express/sample.js
+++ express/sample.js
@@ -1,3 +1,4 @@
+//継続渡しスタイル
 function addAsync(a, b, callback){
     setTimeout(() => callback(a + b), 100);
 }
@@ -6,3 +7,7 @@ console.log("before");
 addAsync(1, 2, result => console.log("Result:" + result));
 
 console.log("after");
+
+//継続渡しではないコールバック
+const result = [1 ,5, 7].map(el => el - 1);
+console.log(result);
